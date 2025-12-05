55b569e2b66213fbfc7f0eb658ae9f73e6d15094
HongliangGDA <hoyu2@s.sfusd.edu>
2025-12-05 13:04:58 -0800

    M5-L10

---
+++ M05.loot_it_all_workbook/item.gd
@@ -0,0 +1,28 @@
+# Script for pickable items. Godot offers great options for composing scenes,
+# and this is a good example of that.
+#
+# It's a plain script we can attach to any Area2D node with a child Sprite2D
+# node, and the script will make the item float and disappear when the player
+# touches it.
+extends Area2D
+
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	play_floating_animation()
+
+
+func play_floating_animation() -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+
+	var sprite_2d := get_node("Sprite2D")
+	var position_offset := Vector2(0.0, 4.0)
+	var duration = randf_range(0.8, 1.2)
+	tween.tween_property(sprite_2d, "position", position_offset, duration)
+	tween.tween_property(sprite_2d, "position",  -1.0 * position_offset, duration)
+	tween.set_loops()
+
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	queue_free()
